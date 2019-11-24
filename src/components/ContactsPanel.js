55404d5c0b80555098bbd97cf898ba6836a8d493
Naval <viveration33@gmail.com>
2019-11-24 05:16:54 +0300

    plakala polovina marshrutki (import svg)

--- src/components/ContactsPanel.js
+++ src/components/ContactsPanel.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import ChatBubble from './ChatBubble';
+import { ReactComponent as Create } from '../svg/create.svg';
 import styles from '../styles/ContactsPanelStyles.module.css';
 
 export default function ContactsPanel(props) {
@@ -80,20 +81,7 @@ export default function ContactsPanel(props) {
 		<div className={styles.ContactsPanel} style={display[props.isChatOpen]} ref={chatRef}>
 			{chats}
 			<button type="button" className={styles.createChat} onClick={handleCreateChat}>
-				<svg x="0px" y="0px" width="40px" height="40px" viewBox="0 0 35.738 35.738">
-					<g>
-						<g>
-							<path
-								d="M0,35.667c0,0,11.596-37.07,35.738-35.55c0,0-2.994,4.849-10.551,6.416c0,0,3.518,0.429,6.369-0.522   
-							c0,0-1.711,5.515-11.025,6.273c0,0,5.133,1.331,7.414,0.57c0,0-0.619,4.111-10.102,6.154c-0.562,0.12-4.347,1.067-1.306,1.448   
-							c0,0,4.371,0.763,5.514,0.381c0,0-3.744,5.607-12.928,5.132c-0.903-0.047-1.332,0-1.332,0L0,35.667z"
-								data-original="#000000"
-								data-old_color="#000000"
-								fill="#D60BDD"
-							/>
-						</g>
-					</g>
-				</svg>
+				<Create className={styles.create} />
 			</button>
 		</div>
 	);
--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,13 +1,12 @@
 import { Link } from 'react-router-dom';
 import React, { useState } from 'react';
+import { ReactComponent as BackArrow } from '../svg/arrow.svg';
+import { ReactComponent as ProfileDefault } from '../svg/profile.svg';
 import styles from '../styles/UserProfileStyles.module.css';
 
 export default function UserProfile(props) {
 	const [values, setValue] = useState('hint');
 	const hintpool = ['Minimum length is 5 characters', 'Good name!'];
-	const inf = React.createRef();
-	const un = React.createRef();
-	const fn = React.createRef();
 	let fullname = '';
 	let username = '';
 	let information = '';
@@ -65,20 +64,7 @@ export default function UserProfile(props) {
 			<div className={styles.userprofileHead}>
 				<Link to={`/`}>
 					<button type="button" className={`${styles.userprofileButton}`}>
-						<svg x="0px" y="0px" viewBox="0 0 492 492" width="40px" height="40px">
-							<g>
-								<g fill="#FFF">
-									<path
-										d="M464.344,207.418l0.768,0.168H135.888l103.496-103.724c5.068-5.064,7.848-11.924,7.848-19.124
-										c0-7.2-2.78-14.012-7.848-19.088L223.28,49.538c-5.064-5.064-11.812-7.864-19.008-7.864c-7.2,0-13.952,2.78-19.016,7.844
-										L7.844,226.914C2.76,231.998-0.02,238.77,0,245.974c-0.02,7.244,2.76,14.02,7.844,19.096l177.412,177.412
-										c5.064,5.06,11.812,7.844,19.016,7.844c7.196,0,13.944-2.788,19.008-7.844l16.104-16.112c5.068-5.056,7.848-11.808,7.848-19.008
-										c0-7.196-2.78-13.592-7.848-18.652L134.72,284.406h329.992c14.828,0,27.288-12.78,27.288-27.6v-22.788
-										C492,219.198,479.172,207.418,464.344,207.418z"
-									/>
-								</g>
-							</g>
-						</svg>
+						<BackArrow className={styles.backArrow} />
 					</button>
 				</Link>
 				<div className={styles.edit}>Edit profile</div>
@@ -86,15 +72,11 @@ export default function UserProfile(props) {
 
 			<div className={styles.userprofileForm}>
 				<div className={styles.avatar}>
-					<svg x="0px" y="0px" viewBox="0 0 1000 1000" width="100px" heigth="100px">
-						<g fill="#8E24AA">
-							<path d="M500,8.8C229.4,8.8,10,228.7,10,500c0,271.3,219.4,491.2,490,491.2c270.6,0,490-219.9,490-491.2C990,228.7,770.6,8.8,500,8.8z M794.8,767.5c-57.7-28-36.5-6.1-111.9-37.2c-77.2-31.8-95.5-42.2-95.5-42.2l-0.7-72.9c0,0,28.9-21.8,37.9-90.8c18,5.2,24.2-21.1,25.1-37.8c1.1-16.2,10.7-66.7-11.4-62.2c4.5-33.7,8.1-64.2,6.5-80.4c-5.5-56.6-44.9-115.8-144.3-120.1c-84.5,4.3-139.3,63.5-144.8,120.2c-1.6,16.1,1.7,46.6,6.2,80.4c-22.1-4.6-12.6,46-11.6,62.2c1.1,16.8,7,43.1,25.1,37.9c9,69,37.9,91,37.9,91l-0.7,73.3c0,0-18.3,11.1-95.5,42.9c-75.4,31.1-54.3,7.9-111.9,35.8c-64-70.8-103.3-164.5-103.3-267.5c0-220.4,178.2-399.1,398.1-399.1c219.9,0,398.1,178.7,398.1,399.1C898.1,603.1,858.8,696.7,794.8,767.5z" />
-						</g>
-					</svg>
+					<ProfileDefault className={styles.profileDefault} />
 				</div>
 				<div className={styles.formcontainer}>
 					<div className={styles.tip}>Full name</div>
-					<form className={styles.fullname} onSubmit={handleSubmit} ref={fn}>
+					<form className={styles.fullname} onSubmit={handleSubmit}>
 						<input
 							className={styles.input}
 							type="text"
@@ -108,9 +90,8 @@ export default function UserProfile(props) {
 				</div>
 				<div className={styles.formcontainer}>
 					<div className={styles.tip}>Username</div>
-					<form className={styles.username} onSubmit={handleSubmit} ref={un}>
+					<form className={styles.username} onSubmit={handleSubmit}>
 						<input
-							ref={un}
 							className={styles.input}
 							type="text"
 							value={props.value}
@@ -124,7 +105,7 @@ export default function UserProfile(props) {
 				<div className={styles.hint}>{hintpool[values]}</div>
 				<div className={styles.formcontainer}>
 					<div className={styles.tip}>Bio</div>
-					<form className={styles.information} onSubmit={handleSubmit} ref={inf}>
+					<form className={styles.information} onSubmit={handleSubmit}>
 						<input
 							className={styles.input}
 							type="text"
